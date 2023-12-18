2100cf9ffdc94e6393a0b234c4e7d9f71407907e
fedorkuruts <fedorkuruts@gmail.com>
2023-12-18 16:46:07 +0600

    kill models

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from backend.swagger_shema import schema_view
 urlpatterns = [
     path('crystal/admin/', admin.site.urls),
     path('crystal/api/', include('backend_api.urls')),
-    path('api/v1/auth/', include('authorization.urls')),
+    # path('api/v1/auth/', include('authorization.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('crystal/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('crystal/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- backend/backend_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TestT
-
-
-urlpatterns = [
-    path("hello/", TestT.as_view(), name='hello_world'),
-]
