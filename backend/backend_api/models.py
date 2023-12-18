100f8f29d4da3930a5e57b92fa0c4d20032b0129
fedorkuruts <fedorkuruts@gmail.com>
2023-12-18 21:37:32 +0600

    init

--- backend/backend_api/models.py
+++ backend/backend_api/models.py
@@ -36,3 +36,6 @@ class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image_file = models.ImageField(upload_to='product_images/')
     visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Image for {self.product.title}"
--- backend/backend_api/serializers.py
+++ backend/backend_api/serializers.py
@@ -1,38 +1,17 @@
-from django.db import models
-from django.utils.text import slugify
-from django.urls import reverse
+from rest_framework import serializers
+from .models import Category, Product, ProductImage
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, blank=True)
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
 
-    def get_absolute_url(self):
-        return reverse('category_detail', args=[self.slug])
-
-    def __str__(self):
-        return self.name
-
-class Product(models.Model):
-    title = models.CharField(max_length=100)
-    menu = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    weight = models.DecimalField(max_digits=10, decimal_places=2)
-    energy = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    visible = models.BooleanField(default=True)
-    is_new = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-
-class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image_file = models.ImageField(upload_to='product_images/')
-    visible = models.BooleanField(default=True)
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = '__all__'
