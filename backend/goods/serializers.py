604e642e97bb5b28cd0b619bb7b44364a416020d
fedorkuruts <fedorkuruts@gmail.com>
2023-12-21 02:32:38 +0600

    the https on images now

--- backend/goods/serializers.py
+++ backend/goods/serializers.py
@@ -16,6 +16,11 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image_file = serializers.SerializerMethodField()
+
+    def get_image_file(self, obj):
+        return self.context['request'].build_absolute_uri(obj.image_file.url).replace('http://', 'https://')
+
     class Meta:
         model = ProductImage
         fields = '__all__'
