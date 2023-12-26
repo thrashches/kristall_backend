87e52294735dcfba77dc044c97448913c2ce61d4
fedorkuruts <fedorkuruts@gmail.com>
2023-12-26 07:48:23 +0600

    a litle more

--- backend/backend_api/urls.py
+++ backend/backend_api/urls.py
@@ -1,5 +1,3 @@
-from django.contrib import admin
-from django.urls import path
 from rest_framework.routers import SimpleRouter
 from authorization.urls import auth_url_paterns
 from authorization.views import ChangeUserDataViewSet
@@ -8,13 +6,9 @@ from backend_api.views import CategoryViewSet, ProductViewSet,  OrderViewSet
 my_router = SimpleRouter()
 my_router.register(r'menu', CategoryViewSet, basename='menu')
 my_router.register(r'goods', ProductViewSet, basename='products')
-my_router.register(r'mybasket',OrderViewSet, basename='basket')
-
+my_router.register(r'basket',OrderViewSet, basename='basket')
 my_router.register(r'users', ChangeUserDataViewSet, basename='chane_user_data')
 
-urlpatterns = [
-      # path('basket' , creataOrderView.as_view(), name='create'),
-    ]
-
+urlpatterns = []
 urlpatterns += my_router.urls
 urlpatterns += auth_url_paterns
